c1aef64d8be814b3e650666d16340a82b156072d
Bruno Ferreira <beffes@gmail.com>
2020-01-19 19:50:25 +0000

    initial commit

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,31 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id=1, Name="O Cozido", Location = "Marvila", Cuisine = CuisineType.Mexican },
+                new Restaurant {Id=2, Name="Caracois", Location = "Lisboa", Cuisine = CuisineType.Indian },
+                new Restaurant {Id=3, Name="Pizza", Location = "Odivelas", Cuisine = CuisineType.Italian },
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
